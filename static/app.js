56838d406ea7541b657017f26528a4844376c706
json-HB <624172381@qq.com>
2016-07-24 02:31:24 +0800

    Create app.js

---
+++ static/app.js
@@ -0,0 +1,21 @@
+
+define(function (require, exports, module) {
+	var MVC = require('MVC');
+	require('m/slider/slider');
+	require('m/header/header');
+	require("m/baidu-seach/baidu-seach");
+	require("m/hotsearchCon/hotsearchCon");
+	require("m/bodyct/bodyct");
+	require("m/frame-body/govSiteTop");
+	require("m/frame-body/layout-content-slider/leftbannerv2");
+	require("m/frame-body/layout-content-slider/servicetab");
+	require("m/frame-body/layout-content-slider/sliderModule");
+
+	return {
+		// 定义模块的入口
+		init: function () {
+			// 将模块加载进来
+			MVC.install();
+		}
+	}
+})
